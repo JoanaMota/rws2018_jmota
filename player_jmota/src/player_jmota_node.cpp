9acdd69f8f6df99d9a2ec6f670222b323d43326a
Joana <joancarvalhomota@ua.pt>
2018-02-28 23:42:56 +0000

    Improved the player code

--- player_jmota/src/player_jmota_node.cpp
+++ player_jmota/src/player_jmota_node.cpp
@@ -1,49 +1,155 @@
-#include <iostream>                                                                                     
+//System includes
+#include <iostream>
+#include <vector>
 
-class Player
+//Boost includes
+#include <boost/shared_ptr.hpp>
+  
+using namespace std;                                                                               
+
+/**
+ * @brief The namespace of this lib
+ */
+namespace rws_jmota
 {
-    public:
-
-    //Constructor with the same name as the class
-    Player(std::string name)
+    class Player
     {
-        this->name = name;
-    }
+        public:
 
-    //Set team name, if given a correct team name (accessor)
-    int setTeamName(std::string team)
+        //Constructor with the same name as the class
+        Player(std::string name)
+        {
+            this->name = name;
+        }
+
+        int setTeamName(int team_index = 0 /*default value*/)
+        {
+            switch (team_index)
+            {
+                case 0: 
+                    return setTeamName("red"); break;
+                case 1: 
+                    return setTeamName("green"); break;
+                case 2: 
+                    return setTeamName("blue");  break;
+                default: 
+                    std::cout << "wrong team index given. Cannot set team" << std::endl; break;
+            }
+        }
+
+        //Set team name, if given a correct team name (accessor)
+        int setTeamName(std::string team)
+        {
+            if (team=="red" || team=="green" || team=="blue")
+            {
+                this->team = team;
+                return 1;
+            }
+            else
+            {
+                std::cout << "cannot set team name to " << team << std::endl;
+                return 0;
+            }
+        }
+
+        //Gets team name (accessor)
+        std::string getTeam(void) {return team;}
+
+        //A public atribute
+        std::string name;
+
+        private:
+        std::string team;
+
+    };
+
+    /**
+     * @brief Class myPlayer extends class Player
+     */
+    class myPlayer: public Player
     {
-        if (team=="red" || team=="green" || team=="blue")
+        public: 
+                                                                                                            
+        myPlayer(std::string name, std::string team): Player(name)
         {
-            this->team = team;
-            return 1;
+            setTeamName(team);
         }
-        else
+    };
+
+    /**
+     * @brief Contains a description of a team
+     */
+    class Team
+    {
+        public: 
+        
+        /**
+         * @brief Constructor
+         * @param team_name the team name
+         */
+        Team(string team_name)
         {
-            std::cout << "cannot set team name to " << team << std::endl;
-            return 0;
+            name = team_name; 
+
         }
-    }
 
-    //Gets team name (accessor)
-    std::string getTeam(void) {return team;}
+        /**
+         * @brief Prints the name of the team and the names of all its players
+         */
+        void printTeamInfo(void)
+        {
+            cout << "team is " << name << endl; 
+            cout << "team players are " << name << endl;  
+            //Write code here ...
+        }
 
-    //A public atribute
-    std::string name;
+        /**
+         * @brief Checks if a player belongs to the team
+         * @param player_name the name of the player to check
+         * @return true or false, yes or no
+         */
+        bool playerBelongsToTeam(string player_name)
+        {
+            //write code here ...
+        }
 
-    private:
-    std::string team;
+        /**
+         * @brief The team name
+         */
+        string name;
 
-};
+        /**
+         * @brief A list of the team's player names
+         */
+        vector<string> players;
+
+    };//end of class Team
+
+};  //end of namespace rws_jmota
 
 int main()
 {
 
-    std::string player_name = "jmota";
-    //Creating an instance of class Player
-    Player player(player_name);
-    player.setTeamName("red");
+    // std::string player_name = "jmota";
+    // //Creating an instance of class Player
+    // Player player(player_name);
+
+    // player.setTeamName("red");
+    // player.setTeamName(1);
                                                                                                         
-    std::cout << "player.name is " << player.name << std::endl;
-    std::cout << "team is " << player.getTeam() << std::endl;
+    // std::cout << "player.name is " << player.name << std::endl;
+    // std::cout << "team is " << player.getTeam() << std::endl;
+
+    //Creating an instance of class Player
+    rws_jmota::myPlayer my_player("jmota","green");
+    cout << "my_player.name is " << my_player.name << endl;
+    cout << "team is " << my_player.getTeam() << endl; 
+
+    //Creating an instance of class Team
+    rws_jmota::Team team("blue");
+
+    team.players.push_back("moliveira");
+    team.players.push_back("vsantos");
+      
+    team.printTeamInfo();
 }
